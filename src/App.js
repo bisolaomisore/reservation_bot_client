614c0934b6275dc58e390ed5cd38d1ded92caaf4
Sola <solaocodes@gmai.com>
2019-07-18 14:52:38 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ReservationsList from './components/ReservationsList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ReservationsList/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
